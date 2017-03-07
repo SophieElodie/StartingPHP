0320b84dd7b888308d4cab843ab6ce7a26c85431
Sophie <Sophie@MacBook-de-Sophie.local>
2017-03-07 14:08:24 +0100

    exercices

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> StartingPHP </title>
+</head>
+<body>
+<a href="variable.php"> exercice 1 </a>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,20 @@
+<?php
+$nom = "Sophie";
+echo $nom;
+?>
+
+<?php
+$nom= "LE BERRE";
+$prenom = "Sophie";
+$age = 20;
+echo "$nom </br> $prenom </br>  $age </br>";
+?>
+
+<?php
+$kilométre = 1;
+echo "$kilométre </br>";
+$kilométre = 3;
+echo "$kilométre</br>";
+$kilométre = 125 ;
+echo "$kilométre</br>";
+?>

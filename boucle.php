7782d31138afb19f5e5d44971e551093ad8d8d8b
Sophie <Sophie@MacBook-de-Sophie.local>
2017-03-08 09:58:16 +0100

    exercice 2

--- boucle.php
+++ boucle.php
@@ -11,5 +11,14 @@ for ($i=0; $i <= 10; $i++) {
             echo "$i </br>";
         }
 ?>
+
+<?php
+     $var1 = 0;
+        $var2 = 20;
+        while ($var1 <= 20) {
+            echo "($var1 * $var2 )";
+            $var1 ++;
+        }
+?>
 </body>
 </html>
